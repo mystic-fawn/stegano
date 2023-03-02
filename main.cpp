4a6f1b1383afb5007e28338eccde9428bd1e178d
mystic fawn <112545288+mystic-fawn@users.noreply.github.com>
2023-03-02 18:08:32 +0100

    modif

--- main.cpp
+++ main.cpp
@@ -3,10 +3,11 @@
 #include <fstream>
 
 
+string argument;
 
 int main(int argc, char** argv)
 {
-    Bitmap bmap;
+    /*Bitmap bmap;
     if (argc <= 1)
     {
         bmap.set("ch1.bmp");
@@ -14,10 +15,28 @@ int main(int argc, char** argv)
     else{
         bmap.set(argv[1]);
     }
-    cout << bmap.posPict << endl;
 
     bmap.hideFile("hello.txt");
-    bmap.getFile("secret.txt");
+    bmap.getFile("secret.txt");*/
+
+    Bitmap bmap;
+    if (argc <= 3)
+    {
+        cout << "Need more argument" << endl;
+    }
+    argument = argv[2];
+    if(argument == "hide")
+    {
+        cout << argv[1] << " hide in " << argv[3] << endl;
+        bmap.set(argv[3]);
+        bmap.hideFile(argv[1]);
+    }
+    if(argument == "get" )
+    {
+        cout << argv[3] << " extracted from " << argv[1] << endl;
+        bmap.set(argv[1]);
+        bmap.getFile(argv[3]);
+    }
 
     return 0;
 }
